77d13fb7641449ffcec445c267487f6b9bf5b47e
최성혁 <shyeokchoi@gmail.com>
2022-12-29 16:41:39 +0900

    [Refactor] ShopRepository를 ShopUsecase에서 꺼내서 별도의 파일로 분리

--- KreamWaffleApp/Usecases/ShopUsecase.swift
+++ KreamWaffleApp/Usecases/ShopUsecase.swift
@@ -69,27 +69,3 @@ final class ShopUsecase {
         return self.productList[index]
     }
 }
-
-final class ShopRepository {
-//    func requestProducts(parameters: ProductRequestModel) -> Single<[Product]> {
-//        return Single.create { single in
-//            let url = URL(string: "https://api.themoviedb.org/3/movie/popular")
-//
-//            let headers: HTTPHeaders = [
-//                "Authorization": "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMDMzZWEzYTRhM2RiNmM4ZmE2NDYxNDkzYzA3NGI4YiIsInN1YiI6IjYzNDI1OTY0YTI4NGViMDA3OWM0MTYxZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eV0F9zBLPdjef9gth_012Q3We_jiY_bjLRyuaqS9DkI"
-//            ]
-//
-//            AF.request(url!, method: .get, parameters: parameters, headers: headers)
-//                .responseDecodable(of: ShopModel.self) { [weak self] response in
-//                    switch response.result {
-//                    case .success(let result):
-//                        single(.success(result.results))
-//                    case .failure(let _):
-//                        single(.success([]))
-//                    }
-//                }
-//
-//            return Disposables.create()
-//        }
-//    }
-}
