7712babce6d1a608b782076d6ddc9345b69c2a4b
최성혁 <shyeokchoi@gmail.com>
2022-12-30 19:52:51 +0900

    [Refactor] StyleUsecase가 데이터를 가지고 있고 StyleViewModel은 전달만 하도록 수정

--- KreamWaffleApp/Usecases/StyleUsecase.swift
+++ KreamWaffleApp/Usecases/StyleUsecase.swift
@@ -6,13 +6,29 @@
 //
 
 import Foundation
+import RxCocoa
 
 final class StyleUsecase {
     private let repository: StyleRepository
     
-    init (repository: StyleRepository) {
-        self.repository = repository
+    let stylePostRelay: BehaviorRelay<[StylePost]> = .init(value: [])
+    
+    var stylePostList = [StylePost]() {
+        didSet {
+            self.stylePostRelay.accept(self.stylePostList)
+        }
     }
     
+    init (repository: StyleRepository) {
+        self.repository = repository
+        setTestData()
+    }
     
+    func setTestData() {
+        self.stylePostList = [
+            StylePost(imageSources: ["https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/362px-Cat_August_2010-4.jpg"], id: "postId1", numLikes: 1, content: "첫번째 게시글"),
+            StylePost(imageSources: ["https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Gustav_chocolate.jpg/156px-Gustav_chocolate.jpg"], id: "postId2", numLikes: 2, content: "두번째 게시글"),
+            StylePost(imageSources: ["https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Felis_catus-cat_on_snow.jpg/358px-Felis_catus-cat_on_snow.jpg"], id: "postId3", numLikes: 3, content: "세번째 게시글"),
+        ]
+    }
 }
--- KreamWaffleApp/ViewModels/StyleViewModel.swift
+++ KreamWaffleApp/ViewModels/StyleViewModel.swift
@@ -13,21 +13,10 @@ final class StyleViewModel {
     private let usecase: StyleUsecase
     
     var styleDataSource: Observable<[StylePost]> {
-        // for testing *****************************************
-        let stylePost1 = StylePost(imageSources: ["https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/362px-Cat_August_2010-4.jpg"], id: "postId1", numLikes: 1, content: "첫번째 게시글")
-        let stylePost2 = StylePost(imageSources: ["https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Gustav_chocolate.jpg/156px-Gustav_chocolate.jpg"], id: "postId2", numLikes: 2, content: "두번째 게시글")
-        let stylePost3 = StylePost(imageSources: ["https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Felis_catus-cat_on_snow.jpg/358px-Felis_catus-cat_on_snow.jpg"], id: "postId3", numLikes: 3, content: "세번째 게시글")
-        return Observable.of([stylePost1, stylePost2, stylePost3])
-        // for testing *****************************************
+        return self.usecase.stylePostRelay.asObservable()
     }
     
     init(usecase: StyleUsecase) {
         self.usecase = usecase
     }
-    
-//    func getImageByIndex(index: Int) -> UIImage {
-//        // for testing *****************************************
-//        // 나중에는 Rx써서 바꿔야함.
-//        return images[index]
-//    }
 }
